38a1944e8b946ea34dffabb893c70953496b74ce
Val <hm_verify.hotmail.com>
2021-08-12 13:38:18 +0000

    Mod 6

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,7 +4,7 @@ const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=1b0c6095182d8b2231d8f6aa09c3fe62&query=' + latitude + ',' + longitude + '&units=f'
 
     request({ url, json: true }, (error, response) => {
-        const {error:r_error, current:r_current} = response.body
+        const { error: r_error, current: r_current } = response.body
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (r_error) {
@@ -13,7 +13,6 @@ const forecast = (latitude, longitude, callback) => {
             callback(undefined, {
                 cur: (r_current.weather_descriptions[0] + ". It is currently " + r_current.temperature + " degress out."),
                 addlInfo: ("Feels like " + r_current.feelslike + ". Humidity " + r_current.humidity + "%")
-                }
             })
         }
     })
